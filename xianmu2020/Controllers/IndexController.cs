7df152586d2c4d8fb4ed552f6b469a57a03f58fa
董 武港 <1258038895@qq.com>
2020-04-03 14:06:34 +0800

    test

---
+++ xianmu2020/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xianmu2020.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Findex()
+        {
+            return View();
+        }
+    }
+}
---
+++ xianmu2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xianmu2020.MvcApplication" Language="C#" %>
